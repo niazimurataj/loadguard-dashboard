31cdfa3018863ed0db807853610f9a0bb4241221
niazimurataj <niazi@murataj.com>
2025-10-28 16:08:02 -0700

    fix: roll back layout.js file so vercel deployment works

---
+++ src/app/details/page.jsx
@@ -0,0 +1,10 @@
+/*
+This will be the detail page for each device
+Map showing device location
+State table showing current parameters
+Charts for each parameter over time
+Ability to export or email device data
+
+Double click page (on state table, per device)
+Charts of every data parameter (DynamoDB call, time series)
+*/
---
+++ src/app/home/page.jsx
@@ -0,0 +1,9 @@
+
+
+// Dashboard landing page for the users
+// Initially, this will be a single page for all users - later, one for each company / etc.
+// From minutes 10282025
+/*
+Create shadcn dashboard
+Home page: map (LHS) and state table (RHS)
+*/
